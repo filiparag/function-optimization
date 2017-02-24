69911fd69d8d6c54afc363527651aa94015a6518
Filip Parag <filiparag.com@gmail.com>
2017-02-24 19:22:14 +0100

    FIx

--- differential-evolution.py
+++ differential-evolution.py
@@ -8,6 +8,7 @@ import csv
 
 def target_function(params):
 
+    #Ackley
     result = -20 * exp(-0.2 * sqrt(0.5 * (params[0] ** 2 + params[1] ** 2))) - \
              exp(0.5 * (cos(6.2 * params[0]) + cos(6.2 * params[1]))) + 2.71 + 20
 
@@ -15,6 +16,29 @@ def target_function(params):
     # result = (1.5 - params[0] + params[0] * params[1]) ** 2 + (2.25 - params[0] \
     #          + params[0] * (params[1] ** 2)) ** 2 + \
     #          (2.625 - params[0] + params[0] * (params[1] ** 3)) ** 2
+
+    # Bukin02    [-15, 5] [-3, 3]
+    # result = 100 * (params[1] - 0.01 * params[0] ** 2 +1) + 0.01(params[0] + 10) ** 2
+
+    # AMGM [0,10]
+    # result = 0.5 * ((params[0] + params[1]) - sqrt(params[0] * params[1])) ** 2
+
+    # Sodp [-1, 1]
+    # result =  abs(params[0] * 2) + abs(params[1] * 3)
+
+    # Treecani [-5, 5]
+    # result = params[0] ** 4 +   4 * params[0] ** 3 + 4 * params[0] ** 2 + params[1] ** 2
+
+    # Trigonometric2 [-500, 500]
+    # result = (1 + 8 * (sin(7 * (params[0] - 0.9) ** 2)) ** 2 + 6 * (sin(14 * (params[0] \
+    #          - 0.9) ** 2)) ** 2 + (params[0] - 0.9) ** 2) + (1 + 8 * (sin(7 * (params[1] \
+    #          - 0.9) ** 2)) ** 2 + 6 * (sin(14 * (params[1] - 0.9) ** 2)) ** 2 + \
+    #          (params[1] - 0.9) ** 2)
+
+
+    # Cosine Mixture  [-1, 1]
+    # result = -0.1 * (cos(5 * pi * params[0]) + cos(5 * pi * params[1])) - (params[0] \
+    #          ** 2 + params[1] ** 2)
 
     # Rosenbrock
     # result = 0
@@ -55,7 +79,7 @@ def differential_evolution(cr, f, np, dim, it, b_lo=-1, b_up=1):
             best = a
             best_fitness = target_function(a)
 
-    return target_function(best)
+    return best
 
 
 def run(params):
@@ -71,13 +95,20 @@ def run(params):
 
     for cr in cr_vals:
         for f in f_vals:
+
             average_fitness = 0
+            average_position = [0] * dimensions
+
             for r in range(repetitions):
-                average_fitness += differential_evolution(cr, f, \
-                                   generation_size, dimensions, iterations, \
-                                   b_lower, b_upper)
+                position = differential_evolution(cr, f, \
+                           generation_size, dimensions, iterations, \
+                           b_lower, b_upper)
+                average_fitness += target_function(position)
+                average_position = [sum(x) for x in zip(average_position, position)]
+
             average_fitness /= repetitions
-            fitness.append([cr, f, average_fitness])
+            average_position = [x / repetitions for x in average_position]
+            fitness.append([cr, f, average_fitness, average_position])
 
     return fitness
 
@@ -91,9 +122,9 @@ def init():
     repetitions = 10
     generation_size = 10
     dimensions = 2
-    iterations = 20
-    b_lower = -1
-    b_upper = 1
+    iterations = 50
+    b_lower = -5
+    b_upper = 5
 
     worker_tasks = []
     for core in range(cpu_cores):
@@ -123,10 +154,17 @@ def init():
         for result in core:
             results.append(result)
 
+    best_position = results[0]
+    for r in results:
+        if r[2] < best_position[2]:
+            best_position = r
+
+    print(best_position)
+
     with open('results.csv', 'w', newline='\n') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ', quotechar=';', quoting=csv.QUOTE_MINIMAL)
         for line in range(len(results)):
-            writer.writerow(results[line])
+            writer.writerow(results[line][:3])
 
 if __name__ == '__main__':
     init()
